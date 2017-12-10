e1f25e2ce38ca0ac15280950b53a2a9b1df59c59
zxj <zxj@zxjs-MacBook-Pro-7.local>
2017-12-10 13:55:55 +0800

    add logback

---
+++ logback/src/main/java/com/logback/LogApplication.java
@@ -0,0 +1,27 @@
+package com.logback;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApplication implements CommandLineRunner{
+
+
+    private final static Logger logger= LoggerFactory.getLogger(LogApplication.class);
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogApplication.class, args);
+    }
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        logger.error("=========hello World========");
+
+        logger.debug("=========hello World========");
+    }
+}
