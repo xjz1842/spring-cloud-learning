983d6aa5d9363d70486738712eb94a42dec92aab
zxj <zhanxuejun@unisound.com>
2018-12-07 13:56:47 +0800

    升级cloud的版本到Finchley.SR2

--- config-server/src/main/java/com/springcloud/config/server/Application.java
+++ config-server/src/main/java/com/springcloud/config/server/Application.java
@@ -3,7 +3,6 @@ package com.springcloud.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
